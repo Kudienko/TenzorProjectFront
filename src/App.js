024009d20aaf90a70431e2c59dc605c0708fd751
ImpulsVL <stiepochkinv@mail.ru>
2024-04-27 01:38:59 +0500

    Сделал item и новый компонент

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import AuthRootComponent from './components/auth/AuthRootComponent';
 import PrivateRoute from './utils/router/PrivateRoute';
 import MainPage from './components/mainPage/MainPage';
+import { ListItem } from '@mui/material';
+
+import Item2 from './components/Item2/Item2.jsx';
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <Routes>
         <Route element={<PrivateRoute />}>
           <Route path='/' element={<MainPage />} />
+          <Route path='item2' element={<Item2 />} />
         </Route>
         <Route path='login' element={<AuthRootComponent />} />
         <Route path='register' element={<AuthRootComponent />} />
