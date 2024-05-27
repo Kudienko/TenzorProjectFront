4c48589e39b484135f7741ee3d2fb5a9f0bdf7c4
ImpulsVL <stiepochkinv@mail.ru>
2024-05-28 02:21:42 +0500

    пусть будет таск #06V

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,19 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./MainPage.scss";
 import SearchItem from "./searchItem/SearchItem";
 import CurrentPlate from "./plates/CurrentPlate";
 import NextPlate from "./plates/NextPlate";
-import {Modal} from './modal/Modal';
-import {useDispatch} from "react-redux";
-import {getWeatherThunk} from "../../store/thunks/getWeatherThunk/getWeatherThunk";
+import { Modal } from './modal/Modal';
+import { ModalAcc } from './modalAccount/ModalAccount';
+import { useDispatch } from "react-redux";
+import { getWeatherThunk } from "../../store/thunks/getWeatherThunk/getWeatherThunk";
 import cloudyVideo from '../../assets/weather/cloudly.mp4';
 import sunnyVideo from '../../assets/weather/sunny.mp4';
 import rainVideo from '../../assets/weather/rain.mp4';
 import snowyVideo from '../../assets/weather/snowy.mp4';
 import windyVideo from '../../assets/weather/windy.mp4';
 import stormVideo from '../../assets/weather/storm.mp4';
+import { ReactComponent as ClothIcon } from '../../assets/btn.svg';
+import { ReactComponent as AccIcon } from '../../assets/acc.svg';
 
-import {ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MainPage() {
@@ -31,6 +34,7 @@ function MainPage() {
     const dispatch = useDispatch();
     const [city, setCity] = useState("Москва");
     const [modalInfoIsOpen, setmodalInfoIsOpen] = useState(false);
+    const [modalAccIsOpen, setModalAccIsOpen] = useState(false);
     const [weatherData, setWeatherData] = useState([]);
     const [selectedDate, setSelectedDate] = useState(null);
     const [backgroundVideo, setBackgroundVideo] = useState(sunnyVideo); // Установить солнечное видео по умолчанию
@@ -66,7 +70,7 @@ function MainPage() {
     const handleCityChange = async (cityName, lat, lon) => {
         setCity(cityName);
         try {
-            const weather = await dispatch(getWeatherThunk({lat, lon}));
+            const weather = await dispatch(getWeatherThunk({ lat, lon }));
 
             if (weather && weather.payload && weather.payload.data) {
                 setWeatherData(weather.payload.data);
@@ -90,9 +94,14 @@ function MainPage() {
                 onClose={() => setmodalInfoIsOpen(false)}
             />
 
+            <ModalAcc
+                isOpen={modalAccIsOpen}
+                onClose={() => setModalAccIsOpen(false)}
+            />
+
             {backgroundVideo && (
                 <video key={backgroundVideo} autoPlay loop muted className="background-video">
-                    <source src={backgroundVideo} type="video/mp4"/>
+                    <source src={backgroundVideo} type="video/mp4" />
                 </video>
             )}
             <div className="content">
@@ -100,22 +109,27 @@ function MainPage() {
                     <div className="city-container">
                         <div className="city">{city}</div>
                     </div>
-                    <SearchItem setCity={setCity} handleCityChange={handleCityChange}/>
+                    <SearchItem setCity={setCity} handleCityChange={handleCityChange} />
+                    <div className="button_account">
+                        <button className="round_button2" onClick={() => setModalAccIsOpen(true)}>
+                            <AccIcon />
+                        </button>
+                    </div>
                 </header>
                 <div className="main_wrapper">
                     {/*<NextPlate data={weatherData} onDateClick={handleDateClick} selectedDate={selectedDate}/>*/}
                     {weatherData.length > 0 ? <NextPlate data={weatherData} onDateClick={handleDateClick}
-                                                     selectedDate={selectedDate}/> : <div className="next_info_no_data">
+                        selectedDate={selectedDate} /> : <div className="next_info_no_data">
                         <div className="no_data_text">Нет данных</div>
                     </div>}
                     {/*<CurrentPlate setmodalInfoIsOpen={setmodalInfoIsOpen} data={selectedData}/>*/}
-                    {selectedData ? <CurrentPlate setmodalInfoIsOpen={setmodalInfoIsOpen} data={selectedData}/> :
+                    {selectedData ? <CurrentPlate setmodalInfoIsOpen={setmodalInfoIsOpen} data={selectedData} /> :
                         <div className="current_day_wrapper_no_data">
                             <div className="no_data_text">Нет данных</div>
                         </div>}
                 </div>
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
     );
 }
