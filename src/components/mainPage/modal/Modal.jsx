6781188d8a480797d275b3f8d9ae4360769adfdd
PC_ilya <kudienko2016@mail.ru>
2024-05-29 12:47:31 +0500

    modal

--- src/components/mainPage/modal/Modal.jsx
+++ src/components/mainPage/modal/Modal.jsx
@@ -20,9 +20,9 @@ import TshirtMale from '../../../assets/clothes/man/summer/jacket.svg'
 import jacketMale from '../../../assets/clothes/man/winter/jacket.svg'
 //legs
 import shortsMale from '../../../assets/clothes/man/summer/trousers.svg'
-import winterTrousersMale '../../../assets/clothes/man/winter/trousers.svg'
-import springTrousersMale '../../../assets/clothes/man/spring/trousers.svg'
-import autumnTrousersMale '../../../assets/clothes/man/autumn/trousers.svg'
+import winterTrousersMale from '../../../assets/clothes/man/winter/trousers.svg'
+import springTrousersMale from '../../../assets/clothes/man/spring/trousers.svg'
+import autumnTrousersMale from '../../../assets/clothes/man/autumn/trousers.svg'
 //feet
 import bootsMale from '../../../assets/clothes/man/autumn/shoes.svg'
 import sneakersMale from '../../../assets/clothes/man/spring/shoes.svg'
@@ -41,8 +41,16 @@ import winterCoatFemale from '../../../assets/clothes/woman/winter/jacket.svg'
 import sweater from '../../../assets/clothes/woman/spring/jacket.svg'
 import coatFemale from '../../../assets/clothes/woman/autumn/jacket.svg'
 //legs
-import skirt from '../../../assets/clothes/woman/summer/'
+import trousersFemale from '../../../assets/clothes/woman/autumn/trousers.svg'
+import jeansFemale from '../../../assets/clothes/woman/spring/trousers.svg'
+import shortsFemale from '../../../assets/clothes/woman/summer/trousers.svg'
+import skirtFemale from '../../../assets/clothes/woman/winter/trousers.svg'
 //feet
+import autumnBootsFemale from '../../../assets/clothes/woman/autumn/shoes.svg'
+import sneakersFemale from '../../../assets/clothes/woman/spring/shoes.svg'
+import slippersFemale from '../../../assets/clothes/woman/summer/shoes.svg'
+import winterBootsFemale from '../../../assets/clothes/woman/winter/shoes.svg'
+
 const maleIcons = {
     head: {
         'Кепка': cap,
@@ -56,7 +64,6 @@ const maleIcons = {
         'Куртка': jacketMale,
     },
     legs: {
-        'Брюки': trousersMale,
         'Шорты': shortsMale
     },
     feet: {
@@ -74,17 +81,19 @@ const femaleIcons = {
         'Шапка': winterHatFemale,
     },
     body: {
-        'Топ': jacketFemale
+        'Топ': top,
     },
     legs: {
-        'Шорты': trousersFemale,
-        'Юбка': trousersFemale,
-        'Брюки': trousersFemale
+        'Шорты': shortsFemale,
+        'Юбка': skirtFemale,
+        'Брюки': trousersFemale,
+        'Джинсы': jeansFemale,
     },
     feet: {
-        'Кеды': shoesFemale,
-        'Тапочки': shoesFemale,
-        'Кроссовки': shoesFemale
+        'Тапочки': slippersFemale,
+        'Кроссовки': sneakersFemale,
+        'Ботинки': autumnBootsFemale,
+        'Утепленные ботинки': winterBootsFemale,
     }
 };
 
@@ -110,17 +119,17 @@ export const Modal = ({ isOpen, onClose, clothes }) => {
                                     <div className="B"><img src={male} alt="" className='gender'/></div>
                                     <div className="C"><img src={female} alt="" className='gender'/></div>
                                     <div className="C"><img src={iconhead} alt="" className='bodyPart'/></div>
-                                    <div className="C"><img src={headdressMale} alt=""/>{clothes.male.head[0]}</div>
-                                    <div className="D"><img src={headdressFemale} alt="" />{clothes.female.head[0]}</div>
+                                    <div className="C"><img src={maleIcons.head[clothes.male.head[0]]} alt=""/></div>
+                                    <div className="D"><img src={femaleIcons.head[clothes.female.head[0]]} alt="" /></div>
                                     <div className="D"><img src={icontorso} alt="" className='bodyPart' /></div>
-                                    <div className="D"><img src={jacketMale} alt="" />{clothes.male.body[0]}</div>
-                                    <div className="D"><img src={jacketFemale} alt="" />{clothes.female.body[0]}</div>
+                                    <div className="D"><img src={maleIcons.body[clothes.male.body[0]]} alt="" /></div>
+                                    <div className="D"><img src={femaleIcons.body[clothes.female.body[0]]} alt="" /></div>
                                     <div className="E"><img src={iconlegs} alt="" className='bodyPart' /></div>
-                                    <div className="E"><img src={trousersMale} alt="" />{clothes.male.legs[0]}</div>
-                                    <div className="D"><img src={trousersFemale} alt="" />{clothes.female.legs[0]}</div>
+                                    <div className="E"><img src={maleIcons.legs[clothes.male.legs[0]]} alt="" /></div>
+                                    <div className="D"><img src={femaleIcons.legs[clothes.female.legs[0]]} alt="" /></div>
                                     <div className="E"><img src={iconfeet} alt="" className='bodyPart'/></div>
-                                    <div className="E"><img src={shoesMale} alt="" />{clothes.male.feet[0]}</div>
-                                    <div className="D"><img src={shoesFemale} alt="" />{clothes.female.feet[0]}</div>
+                                    <div className="E"><img src={maleIcons.feet[clothes.male.feet[0]]} alt="" /></div>
+                                    <div className="D"><img src={femaleIcons.feet[clothes.female.feet[0]]} alt="" /></div>
                                 </div>
                             </div>
                         </div>
